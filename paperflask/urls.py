4769f1a3a9e217a85a77db07efacedb20ac7bde3
antiflee <antiflee@gmail.com>
2017-11-06 02:02:25 -0500

    modify static_root in settings.py

--- paperflask/urls.py
+++ paperflask/urls.py
@@ -26,6 +26,5 @@ urlpatterns = [
     url(r'^$', home, name='home'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
+urlpatterns += (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
     # urlpatterns += (static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
