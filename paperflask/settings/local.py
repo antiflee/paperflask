7edcbf794e39e66c95e3a2b7016299df154aa00f
antiflee <antiflee@gmail.com>
2017-11-06 01:06:51 -0500

    before deployment

--- paperflask/settings/local.py
+++ paperflask/settings/local.py
@@ -129,4 +129,10 @@ STATICFILES_DIRS = [
 # will be served
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static-serve")
 
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
